e2bde739875efa4c5486ec95adac6d1e1167cdb5
Heather Shapiro <hshapiro93@gmail.com>
2016-11-04 10:20:29 -0400

    updated createbasevm

--- Python/createbasevm.py
+++ Python/createbasevm.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 ##pip3 install --pre azure
 ##pip4 install msrest
 ##pip3 install msrestazure
 
 
-# In[13]:
+# In[42]:
 
 import json
 
@@ -19,7 +19,6 @@ from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.storage import StorageManagementClient
 
 from azure.storage import CloudStorageAccount
-# from azure.mgmt.resource.resources.models import ContentSettings
 
 from azure.mgmt.resource.resources.models import ResourceGroup
 
@@ -33,13 +32,59 @@ from azure.mgmt.resource.resources.models import ParametersLink
 from azure.mgmt.resource.resources.models import TemplateLink
 
 
-# In[5]:
+# In[78]:
 
-with open("az_config.json") as data_file:
+from tkinter import *
+import tkinter.messagebox as tm
+import json
 
-    data = json.load(data_file)
 
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        tk.Label(root,text="Please enter your Azure Credentials").pack()
+        self.label_1 = Label(self, text="Username")
+        self.label_2 = Label(self, text="Password")
+        self.label_3 = Label(self, text="Subscription Id")
+
+        self.entry_1 = Entry(self)
+        self.entry_2 = Entry(self, show="*")
+        self.entry_3 = Entry(self)
+
+        self.label_1.grid(row=0, sticky=E)
+        self.label_2.grid(row=1, sticky=E)
+        self.label_3.grid(row=2,sticky=E)
+        self.entry_1.grid(row=0, column=1)
+        self.entry_2.grid(row=1, column=1)
+        self.entry_3.grid(row=2,column=1)
+
+#         self.checkbox = Checkbutton(self, text="Save Credentials")
+#         self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Save", command = self._login_btn_clickked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clickked(self):
+        
+        username = self.entry_1.get()
+        password = self.entry_2.get()
+        subscription_id = self.entry_3.get()
+        
+        data = {"username":username, "password":password, "subscription_id":subscription_id}
+#         print(data)
+        filename = "az_config.json"
+        with open(filename, 'w+') as temp_file:
+            json.dump(data,temp_file)
+        
+        
+        root.destroy()
     
+
+
+# In[43]:
 
 def get_credentials(config_data):
 
@@ -55,24 +100,50 @@ def get_subscription(config_data):
     return config_data["subscription_id"]
 
 
-# In[8]:
+# In[46]:
+
+##prompt for credentials
+
+from pathlib import Path
+
+my_file = Path("az_config.json")
+if not my_file.is_file():
+    root = Tk()
+    lf = LoginFrame(root)
+    root.mainloop()
+with open("az_config.json") as data_file:
+    data = json.load(data_file)
+
+    
+    
+
+
+
+# In[82]:
+
+##get credentials
 
 credentials = get_credentials(data)
 subscription_id = get_subscription(data)
 print("Creds have been delivered from:", credentials.cred_store)
 
 
-# In[15]:
+# In[83]:
+
+#set up RM client
 
 client = ResourceManagementClient(
     credentials, 
     subscription_id
 )
+print("The client was set up")
 
 
-# In[17]:
+# In[84]:
 
-group_name = 'TestRG1'
+##create resource group
+
+group_name = 'TestRG3'
 
 resource_group_params = {'location':'eastus'}
 
@@ -82,12 +153,16 @@ client.resource_groups.create_or_update(group_name, resource_group_params)
 print ("Created Resource Group:", group_name)
 
 
-# In[18]:
+# In[85]:
+
+##create deployment name
 
 deployment_name = 'testvm'
 
 
-# In[20]:
+# In[86]:
+
+##create deployment using templatelink
 
 template = TemplateLink(
 
@@ -102,8 +177,7 @@ parameters = ParametersLink(
 
 )
 
-
-result = client.deployments.create_or_update(
+client.deployments.create_or_update(
 
     group_name,
 
@@ -121,9 +195,4 @@ result = client.deployments.create_or_update(
 
 )
 print("Created Deployment:", deployment_name)
-
-
-# In[31]:
-
-
 
