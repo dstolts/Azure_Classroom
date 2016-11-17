697fd01705b183e3b046b0cb8f760c1d06524684
Dan Stolts <dstolts@microsoft.com>
2016-11-17 17:02:40 -0500

    Update createbasevm.ps1
    Changed to generic name

--- createbasevm.ps1
+++ createbasevm.ps1
@@ -1,5 +1,5 @@
 #Azure CLI for quick create 
-azure vm quick-create -g MITClass -n basevm "eastus" -y Linux -Q canonical:ubuntuserver:14.04.2-LTS:latest -u tdradmin -M id_rsa.pub
+azure vm quick-create -g Class -n basevm "eastus" -y Linux -Q canonical:ubuntuserver:14.04.2-LTS:latest -u tdradmin -M id_rsa.pub
 
 $vm = azure vm show mitclass basevm --json | Convertfrom-Json
 $nic_id = $vm.networkProfile.networkInterfaces[0] | Select-Object -ExpandProperty id
