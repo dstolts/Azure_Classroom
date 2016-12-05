6b89763aa9af4e55ea1b4bfabfd7af10a6ce5acf
Jessica Deen <jedeen@outlook.com>
2016-12-05 12:51:06 -0800

    Cleanup PS in Git

--- login.ps1
+++
@@ -1,7 +0,0 @@
-#Azure CLI Login
-Write-Output "Please follow the prompt to login to Azure CLI."
-Azure login
-
-#Azure PowerShell Login
-Write-Output "Please follow the prompt to login to Azure PowerShell."
-Add-AzureRMAccount
