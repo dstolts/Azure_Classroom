aa317c377e016a28dcb3da95bca32bf62dd99d50
Heather Shapiro <hshapiro93@gmail.com>
2016-12-06 13:12:56 -0500

    updated azure json

--- Python/createbasevm.py
+++ Python/createbasevm.py
@@ -1,14 +1,11 @@
-
-# coding: utf-8
-
 import pip
+
 
 def import_or_install(package):
     try:
         __import__(package)
     except ImportError:
-        pip.main(['install', package])  
-
+        pip.main(['install', package])
 
 import_or_install("json")
 import_or_install("azure.common.credentials")
@@ -51,11 +48,13 @@ from pathlib import Path
 from azure.storage.blob import PublicAccess
 import pprint
 
+
 class LoginFrame(Frame):
+
     def __init__(self, master):
         super().__init__(master)
-        
-        Label(root,text="Please enter your Azure Credentials").pack()
+
+        Label(root, text="Please enter your Azure Credentials").pack()
         self.label_1 = Label(self, text="Username")
         self.label_2 = Label(self, text="Password")
         self.label_3 = Label(self, text="Subscription Id")
@@ -66,30 +65,31 @@ class LoginFrame(Frame):
 
         self.label_1.grid(row=0, sticky=E)
         self.label_2.grid(row=1, sticky=E)
-        self.label_3.grid(row=2,sticky=E)
+        self.label_3.grid(row=2, sticky=E)
         self.entry_1.grid(row=0, column=1)
         self.entry_2.grid(row=1, column=1)
-        self.entry_3.grid(row=2,column=1)
+        self.entry_3.grid(row=2, column=1)
 
-        self.logbtn = Button(self, text="Save", command = self._login_btn_clickked)
+        self.logbtn = Button(
+            self, text="Save", command=self._login_btn_clickked)
         self.logbtn.grid(columnspan=2)
 
         self.pack()
 
     def _login_btn_clickked(self):
-        
+
         username = self.entry_1.get()
         password = self.entry_2.get()
         subscription_id = self.entry_3.get()
-        
-        data = {"username":username, "password":password, "subscription_id":subscription_id}
+
+        data = {"username": username, "password": password,
+                "subscription_id": subscription_id}
         filename = "az_config.json"
         with open(filename, 'w+') as temp_file:
-            json.dump(data,temp_file)
-        
-        
+            json.dump(data, temp_file)
+
         root.destroy()
-    
+
 
 def get_credentials(config_data):
 
@@ -101,10 +101,11 @@ def get_credentials(config_data):
 
     )
 
+
 def get_subscription(config_data):
     return config_data["subscription_id"]
 
-##prompt for credentials
+# prompt for credentials
 
 
 my_file = Path("az_config.json")
@@ -115,57 +116,55 @@ if not my_file.is_file():
 with open("az_config.json") as data_file:
     data = json.load(data_file)
 
-    
 
-##get credentials
+# get credentials
 
 credentials = get_credentials(data)
 subscription_id = get_subscription(data)
 print("Creds have been delivered from:", credentials.cred_store)
 
 
-#set up RM client
+# set up RM client
 
 client = ResourceManagementClient(
-    credentials, 
+    credentials,
     subscription_id
 )
 print("The client was set up")
 
 
-##create resource group
+# create resource group
 
 group_name = 'TestRG'
 
-resource_group_params = {'location':'eastus'}
+resource_group_params = {'location': 'eastus'}
 
 
-client.resource_groups.create_or_update(group_name, resource_group_params, public_access = PublicAccess.Container)
+client.resource_groups.create_or_update(
+    group_name, resource_group_params, public_access=PublicAccess.Container)
 
 
-print ("Created Resource Group:", group_name)
+print("Created Resource Group:", group_name)
 
 
-
-##create deployment name
+# create deployment name
 
 deployment_name = 'testvm'
 
 
 # In[86]:
 
-##create deployment using templatelink
+# create deployment using templatelink
 
 template = TemplateLink(
-
-    uri = 'https://raw.githubusercontent.com/dstolts/Azure_Classroom/master/Python/azuredeploy.json'
+    uri='https://raw.githubusercontent.com/dstolts/Azure_Classroom/master/Python/azuredeploy.json',
 )
 
-# parameters = ParametersLink(
+parameters = ParametersLink(
 
-#     uri='https://raw.githubusercontent.com/dstolts/Azure_Classroom/master/Python/azuredeploy.parameters.json',
+    uri='https://raw.githubusercontent.com/dstolts/Azure_Classroom/master/Python/azuredeploy.parameters.json',
 
-# )
+)
 
 result = client.deployments.create_or_update(
 
@@ -178,10 +177,8 @@ result = client.deployments.create_or_update(
         mode=DeploymentMode.incremental,
 
         template_link=template,
-
-        # parameters_link=parameters,
-        parameters = {
-            "adminUserName": {
+        parameters={
+            "adminUsername": {
                 "value": "schoolstudent"
             },
             "adminPassword": {
@@ -189,8 +186,9 @@ result = client.deployments.create_or_update(
             },
             "dnsLabelPrefix": {
                 "value": "classroomTest"
-                }
             }
+        }
+        # parameters_link=parameters,
     )
 
 )
@@ -200,5 +198,3 @@ print("Created Deployment:", deployment_name)
 print(deployment_name + " is being deployed...")
 result.wait()
 print("The deployment finished successfully")
-
-
